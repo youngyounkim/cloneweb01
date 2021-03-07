b6a846ba26268f805db1a5be6871b907db6b225d
youngyounkim <rladuddbs789@gmail.com>
2021-03-07 11:32:56 +0900

    header commit

--- render.js
+++ render.js
@@ -1,25 +1,30 @@
 
-
 let liList = [
-    { name : "SHOP", sub : ["WORDPRESS", "SQUARESPACE"]},
-    { name : "INSTALLATION", sub : ["WORDPRESS", "SQUARESPACE"]},
-    { name : "ABOUT"},
-    { name : "SHOWCASE"},
-    { name : "BLOG"}    
+    { name : "SHOP", sub : ["WORDPRESS", "SQUARESPACE"], link : "#"},
+    { name : "INSTALLATION", sub : ["WORDPRESS", "SQUARESPACE"], link : "#"},
+    { name : "ABOUT", link : "#"},
+    { name : "SHOWCASE", link : "#"},
+    { name : "BLOG", link : "#"}    
 ]
 
 render = () => {
     const ul = document.getElementById("menu");
-    console.log(ul)
     for(let el of liList){
         let li = document.createElement("li");
-        li.textContent = el.name;        
+        let a = document.createElement("a")
+        a.textContent = el.name;
+        a.href = el.link;
+        li.appendChild(a);
+        li.classList.add("menuLi")
         if(el.sub){
             let subul = document.createElement("ul");
             subul.classList.add("subUl")
             for(let i = 0; i < el.sub.length; i++) {
                 let subLi = document.createElement("li");
-                subLi.textContent = el.sub[i];
+                let suba = document.createElement("a");
+                suba.href = el.link;
+                suba.textContent = el.sub[i];
+                subLi.appendChild(suba);
                 subul.appendChild(subLi);
             }
             li.appendChild(subul);
@@ -27,4 +32,39 @@ render = () => {
         ul.appendChild(li);
     }
 }
+const ul1 = document.getElementsByClassName("hideMenu");
+const ul = ul1[0];
+sideRender = () =>{
+    for(let el of liList){
+        let li = document.createElement("li");
+        let a = document.createElement("a")
+        a.textContent = el.name;
+        a.href = el.link;
+        li.appendChild(a);
+        //li.classList.add("menuLi")
+        if(el.sub){
+            let subul = document.createElement("ul");
+            subul.classList.add("subUl")
+            for(let i = 0; i < el.sub.length; i++) {
+                let subLi = document.createElement("li");
+                let suba = document.createElement("a");
+                suba.href = el.link;
+                suba.textContent = el.sub[i];
+                subLi.appendChild(suba);
+                subul.appendChild(subLi);
+            }
+            li.appendChild(subul);
+        }
+        ul.appendChild(li);
+    }
+}
+
+document.getElementsByClassName("listIcon")[0].addEventListener("click", () => {
+    ul.classList.add("on")
+})
+document.getElementsByClassName("clear")[0].addEventListener("click", () => {
+    ul.classList.remove("on")
+})
+
 render();
+sideRender();
